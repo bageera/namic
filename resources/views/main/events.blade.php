64eb76bca04d5458e73aba1cf01e7963276f5d74
Jon <jon@nocturnalinc.com>
2015-02-22 10:53:26 -0500

    Converted events page

--- resources/views/main/events.blade.php
+++ resources/views/main/events.blade.php
@@ -3,4 +3,102 @@
     NAMIC-Carolinas - Events
 @endsection
 @section(('content'))
+    <div id="container">
+        <div id="content" data-layout="twocolumnc" data-index="23"><div id="grid" class="container_24">
+                <div id="pageTitle" class="page-title">Events</div><br class="page-title" />
+                    <div id="billboard" class="billboard-holder" style="display:none;">
+                        <div class="billboard empty" data-id="0" data-height="300">
+
+                        </div>
+                    </div>
+                <div class="column container grid_24 alpha">
+                    <div class="column grid_12 alpha">
+                        <div class="block heading" data-type="heading" data-title="heading" data-id="0">
+                            <h3>
+                                <div class='h3' style='text-align:left;'>Lunch &amp; Learns
+                                </div>
+                            </h3>
+                        </div>
+                        <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="1">
+                            <img src="{{asset('img/events/685423_1456313_1407944650.jpg')}}" style='float:left;'  class='image' />
+                            <div style="">
+                                Lunch and Learns are designed to give a concentrated informational approach to a “working lunch”.
+                                They were created to give area professionals quick insights into industry updates, career news, and product and skills training,
+                                as well as professional development.
+                                Industry leaders provide advice for professional growth and personal development based on their individual experiences when climbing the corporate ladder.
+                                It is a way to provide specialty training that most times are not offered within a Corporate environment.
+                                Each program is designed based on current industry trends relative to career growth opportunities,
+                                servicing: Charlotte, Greensboro, Raleigh, and South Carolina
+                            </div>
+                        </div>
+                        <div class="block heading" data-type="heading" data-title="heading" data-id="2">
+                            <h3>
+                                <div class='h3' style='text-align:left;'>
+                                    Digital Media Summit
+                                </div>
+                            </h3>
+                        </div>
+                        <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="3">
+                            <img src="{{asset('img/events/685423_1456315_1407944650.jpg')}}" style='float:left;'  class='image' />
+                            <div style="">
+                                The Carolinas Chapter of the National Association of Multi-Ethnicity in Communications (NAMIC)
+                                hosts its annual Digital Media Summit in the Fall in Charlotte, NC. Carol Hevey, the EVP of Operations,
+                                Time Warner Cable East Region  welcomes attendees to an engaging,
+                                half-day event focused on the evolution of digital media and its impact on communications companies.
+                                The event has assembled some of the most innovative minds in digital media and technology to discuss how communications companies
+                                and programmers are leveraging Digital Media to maintain their relevance.
+                            </div>
+                        </div>
+                        <div class="block heading" data-type="heading" data-title="heading" data-id="4">
+                            <h3>
+                                <div class='h3' style='text-align:left;'>
+                                    Career Development
+                                </div>
+                            </h3>
+                        </div>
+                        <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="5">
+                            <img src="{{asset('img/events/685423_1456316_1407944650.jpg')}}" style='float:left;'  class='image' />
+                            <div style="">
+                                Career Development Workshops are an opportunity for NAMIC-Carolinas members, guests,
+                                and the community to come together while partnering with area professionals.
+                                Industry professionals share  their expertise  on such topics as:
+                                Résumés, Interviewing, Networking, Social Media, Business Communications, Diversity and Inclusion, or anything that will assist our attendees in making
+                                the most out of their career choices and potential business opportunities.
+                                NAMIC-Carolinas has taken an active role in mentoring, educating, and advocating the development of our business partners
+                                and community leaders and members by providing these Workshops.
+                            </div>
+                        </div>
+                        <div class="block heading" data-type="heading" data-title="heading" data-id="6"><h3><div class='h3' style='text-align:left;'>Follow Us</div></h3></div><div class="block socialicons" data-type="socialicons" data-title="socialicons" data-id="7"><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='http://www.facebook.com/NAMICCarolinasChapter' target='_blank' class='imageLink'><img src='/resources/site/images/social/facebook-32x32.png' alt='follow on'/></a></div><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='http://twitter.com/#!/NAMICCarolinas' target='_blank' class='imageLink'><img src='/resources/site/images/social/twitter-32x32.png' alt='follow on'/></a></div><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='http://www.linkedin.com/groups/NAMICCarolinas-4141647?home=&gid=4141647&trk=anet_ug_hm' target='_blank' class='imageLink'><img src='/resources/site/images/social/linkedin-32x32.png' alt='follow on'/></a></div></div></div><div class="column grid_12 omega"><div class="block heading" data-type="heading" data-title="heading" data-id="8"><h3><div class='h3' style='text-align:left;'>2014 Event Calendar</div></h3></div><div class="block download" data-type="download" data-title="download" data-id="17"><div align="left"><div class="download-button rollover" align="left" style="padding:7px;display:inline-block;" data-fileID="101980" data-url="http://cloud2.snappages.com/c88cbf3eb295bc8441d9b0852802e528b01137ab/New 2014 NAMIC Program Calendar communication2.pdf" data-type="pdf" >
+                                    <img src="{{asset('img/events/pdf_file_30.png')}}" style="float:left;"/>
+                                    <img src="{{asset('img/events/cloud_download_25.png')}}" style="margin-top:8px;margin-left:-15px;float:left"/>
+                                    <div style="float:left;margin-left:5px;margin-top:-2px;">
+                                        <div style="font-weight:bold;font-size:15px;margin-bottom:-2px;">2014 NAMIC Program Calendar - Updated</div>
+                                        click to download
+                                    </div>
+                                    <div style="clear:both;"></div>
+                                </div></div></div><div class="block text" data-type="text" data-title="text" data-id="21"><div style=""><p><b>SAVE THE DATE ANNOUNCEMENT</b></p>
+
+                                <p><b>First Annual NAMIC-Carolinas Leadership Awards Dinner</b><br>
+                                    Charlotte, North Carolina<br>
+                                    <span>September 9, 2014</span><br>
+                                    <span>7:00 pm</span><br>
+                                    <span>Awards Granted: Carolinas leaders in the Communications industry.</span></p>
+
+                                <p>The EPIC (Exceptional Person in Communications) awards will be our way of recognizing the most outstanding Carolinas Leaders in Communications that support diversity.</p>
+
+                                <p><b>Fifth Annual NAMIC-Carolinas Digital Media Summit. “Digital Presence”</b><br>
+                                    <span>Charlotte, North Carolina</span><br>
+                                    <span>September 10, 2014</span><br>
+                                    <span>Registration: 1:30 and the event will start at 2pm.</span></p>
+
+                                <p>Keynote Speaker: Angela Talton, SVP, Global Diversity &amp; Inclusion at Nielsen. Jim Ludington will be the Moderator for our panel discussion. Networking mixer immediately following the event.</p>
+                            </div></div><div class="block download" data-type="download" data-title="download" data-id="22"><div align="left"><div class="download-button rollover" align="left" style="padding:7px;display:inline-block;" data-fileID="104993" data-url="http://cloud2.snappages.com/c88cbf3eb295bc8441d9b0852802e528b01137ab/NAMIC_flyer_2_8 5x11.pdf" data-type="pdf" >
+                                    <img src="{{asset('img/events/pdf_file_30.png')}}" style="float:left;"/>
+                                    <img src="{{asset('img/events/cloud_download_25.png')}}" style="margin-top:8px;margin-left:-15px;float:left"/>
+                                    <div style="float:left;margin-left:5px;margin-top:-2px;">
+                                        <div style="font-weight:bold;font-size:15px;margin-bottom:-2px;">Save the Date Announcement</div>
+                                        click to download
+                                    </div>
+                                    <div style="clear:both;"></div>
+                                </div></div></div></div></div><div style="clear:both;"></div></div></div>	</div>
 @endsection
