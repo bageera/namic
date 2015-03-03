e045af988f78acc69e83abffacb7a2e0f41decea
Jon <jon@nocturnalinc.com>
2015-03-02 22:53:55 -0500

    added in unplugged page created images for unplugged page created favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,16 +10,17 @@
     <!-- css includes -->
     <link type="text/css" href="{{asset('css/base.css')}}" rel="stylesheet" />
     <link type="text/css" href="{{asset('css/style.css')}}" rel="stylesheet" />
+    <!--
     <link type="text/css" href="/resources/site/snappages.v1.min.css" rel="stylesheet" />
     <link type="text/css" href="/resources/site/utils/slider/jslider.css" rel="stylesheet" />
-
+    -->
     <!-- javascript includes -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-    <!-- <script type="text/javascript" src="http://www.linkedin.com/js/public-profile/widget-os.js"></script> -->
+    <!-- <script type="text/javascript" src="http://www.linkedin.com/js/public-profile/widget-os.js"></script>
     <script type="text/javascript" src="/resources/site/snappages.v1.min.js"></script>
     <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
-
+    -->
 
 
     <script type="text/javascript">
@@ -38,7 +39,9 @@
                 $('.column:not(.container)').find('.inner-wrapper').append($('<div style="clear:both;"></div>'));
             });
         });
-    </script></head>
+    </script>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/x-icon">
+</head>
 
 <body>
 <!-- output body -->
@@ -110,5 +113,15 @@
 </div>
     <!-- end of wrapper -->
 </div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-60321727-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
--- resources/views/main/unplugged.blade.php
+++ resources/views/main/unplugged.blade.php
@@ -4,9 +4,39 @@
 @endsection
 @section(('content'))
     <div id="container">
-        <div id="content" data-layout="twocolumn" data-index="6"><div id="grid" class="container_24"><div id="billboard" class="billboard-holder" style="display:none;"><div class="billboard empty" data-id="0" data-height="300"></div></div><div class="column grid_24 alpha"><div class="block image" data-type="image" data-title="image" data-id="0">			<div align='center'>
-                            <img src="{{asset('img/unplugged/1473201_2172659_1400674529.jpg')}}" align='center'  class='image ' /></div>
-                    </div><div class="block text" data-type="text" data-title="text" data-id="1"><div style="text-align:center;">Issues:<br>
+        <div id="content" data-layout="twocolumn" data-index="6">
+            <div id="grid" class="container_24">
+                <div id="billboard" class="billboard-holder" style="display:none;">
+                    <div class="billboard empty" data-id="0" data-height="300">
+
+                    </div>
+                </div>
+                <div class="column grid_24 alpha">
+                    <div class="block image" data-type="image" data-title="image" data-id="0">
+                        <div align='center'>
+                            <img src="{{asset('img/unplugged/1473201_2172659_1400674529.jpg')}}" align='center'  class='image ' />
+                        </div>
+                    </div>
+                    <div class="block text" data-type="text" data-title="text" data-id="1">
+                        <div style="text-align:center;">Issues:<br>
                             <br>
-                            <a aria-describedby="ui-tooltip-17" href="{{URL::to('issue-1')}}" target="_self">NAMIC-Carolinas UNPLUGGED Issue 1.0</a></div></div><div class="block text" data-type="text" data-title="text" data-id="5"><div style="text-align:center;"><a href="http://cloud2.snappages.com/c88cbf3eb295bc8441d9b0852802e528b01137ab/NAMIC Unplugged 2.0.pdf" target="_blank">NAMIC-Carolinas UNPLUGGED Issues 2.0</a></div></div></div><div style="clear:both;"></div></div></div>	</div>
+                            <a aria-describedby="ui-tooltip-17" href="{{URL::to('issue-1')}}" target="_self">
+                                NAMIC-Carolinas UNPLUGGED Issue 1.0
+                            </a>
+                        </div>
+                    </div>
+                    <div class="block text" data-type="text" data-title="text" data-id="5">
+                        <div style="text-align:center;">
+                            <a href="http://cloud2.snappages.com/c88cbf3eb295bc8441d9b0852802e528b01137ab/NAMIC Unplugged 2.0.pdf" target="_blank">
+                                NAMIC-Carolinas UNPLUGGED Issues 2.0
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style="clear:both;">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
