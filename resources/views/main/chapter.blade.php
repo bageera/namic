f64a7903e5bfcc9bbd3d8e884e47a210d23959c4
Jon <jon@nocturnalinc.com>
2015-03-03 00:21:31 -0500

    updated image links on chapter page

--- resources/views/main/chapter.blade.php
+++ resources/views/main/chapter.blade.php
@@ -5,49 +5,206 @@
 @section(('content'))
 
 <div id="container">
-    <div id="content" data-layout="threecolumn" data-index="46"><div id="grid" class="container_24"><div id="pageTitle" class="page-title">About the Chapter</div><br class="page-title" /><div id="billboard" class="billboard-holder" style="display:none;"><div class="billboard empty" data-id="0" data-height="300"></div></div><div class="column container grid_24 alpha"><div class="column grid_24 alpha"><div class="block heading" data-type="heading" data-title="heading" data-id="31"><h1><div class='h1' style='text-align:left;'>2014 BOARD MEMBERS</div></h1></div></div><div class="column container grid_24 alpha"><div class="column grid_6 alpha"><div class="block image" data-type="image" data-title="image" data-id="21">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347047_1404240589.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="26"><div style=""><b>President</b><br>
-                                Monica L. Moore</div></div></div><div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="22">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347048_1404240589.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="27"><div style=""><b>Vice President</b><br>
-                                Tunza Wallace</div></div></div><div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="23">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347049_1404240589.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="28"><div style=""><b>Treasurer</b><br>
-                                Kellee Evans</div></div></div><div class="column grid_6 omega"><div class="block image" data-type="image" data-title="image" data-id="24">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347050_1404240589.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="29"><div style=""><b>Secretary</b><br>
-                                Danine Prakash</div></div></div></div></div><div class="column container grid_24"><div class="column grid_24 alpha"><div class="block heading" data-type="heading" data-title="heading" data-id="30"><h1><div class='h1' style='text-align:left;'>2014 Advisory Committee</div></h1></div></div><div class="column container grid_24 alpha"><div class="column grid_6 alpha"><div class="block image" data-type="image" data-title="image" data-id="32">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347057_1404240589.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="45"><div style="">Advisor<br>
+    <div id="content" data-layout="threecolumn" data-index="46">
+        <div id="grid" class="container_24">
+            <div id="pageTitle" class="page-title">
+                About the Chapter
+            </div>
+            <br class="page-title" />
+            <div id="billboard" class="billboard-holder" style="display:none;">
+                <div class="billboard empty" data-id="0" data-height="300">
+
+                </div>
+            </div>
+            <div class="column container grid_24 alpha">
+                <div class="column grid_24 alpha">
+                    <div class="block heading" data-type="heading" data-title="heading" data-id="31">
+                        <h1><div class='h1' style='text-align:left;'>
+                                2014 BOARD MEMBERS
+                            </div>
+                        </h1>
+                    </div>
+                </div>
+                <div class="column container grid_24 alpha">
+                    <div class="column grid_6 alpha">
+                        <div class="block image" data-type="image" data-title="image" data-id="21">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347047_1404240589.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="26">
+                            <div style=""><b>President</b><br>
+                                Monica L. Moore
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="22">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347048_1404240589.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="27">
+                            <div style=""><b>Vice President</b><br>
+                                Tunza Wallace
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6">
+                        <div class="block image" data-type="image" data-title="image" data-id="23">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347049_1404240589.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="28">
+                            <div style="">
+                                <b>Treasurer</b><br>
+                                Kellee Evans
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6 omega">
+                        <div class="block image" data-type="image" data-title="image" data-id="24">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347050_1404240589.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="29">
+                            <div style="">
+                                <b>Secretary</b><br>
+                                Danine Prakash
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column container grid_24">
+                <div class="column grid_24 alpha">
+                    <div class="block heading" data-type="heading" data-title="heading" data-id="30">
+                        <h1>
+                            <div class='h1' style='text-align:left;'>
+                                2014 Advisory Committee
+                            </div>
+                        </h1>
+                    </div>
+                </div>
+                <div class="column container grid_24 alpha">
+                    <div class="column grid_6 alpha">
+                        <div class="block image" data-type="image" data-title="image" data-id="32">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347057_1404240589.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="45">
+                            <div style="">Advisor<br>
                                 Darrel Hegar<br>
                                 RVP Operations<br>
-                                Time Warner Cable</div></div><div class="block image" data-type="image" data-title="image" data-id="36">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347062_1404240590.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="41"><div style="">Advisor<br>
+                                Time Warner Cable
+                            </div>
+                        </div>
+                        <div class="block image" data-type="image" data-title="image" data-id="36">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347062_1404240590.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="41">
+                            <div style="">Advisor<br>
                                 Garth David<br>
                                 Immediate Past President<br>
-                                NAMIC-Carolinas</div></div></div><div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="33">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347058_1404240590.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="39"><div style="">Advisor<br>
+                                NAMIC-Carolinas
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6">
+                        <div class="block image" data-type="image" data-title="image" data-id="33">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347058_1404240590.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="39">
+                            <div style="">Advisor<br>
                                 Charlon McIntosh<br>
                                 RVP, BC Customer Care<br>
-                                Time Warner Cable</div></div><div class="block image" data-type="image" data-title="image" data-id="37">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347063_1404240590.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="42"><div style="">Advisor<br>
+                                Time Warner Cable
+                            </div>
+                        </div>
+                        <div class="block image" data-type="image" data-title="image" data-id="37">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347063_1404240590.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="42">
+                            <div style="">Advisor<br>
                                 Khanspasha Feaster<br>
                                 Sr. Director, Leader Dev<br>
-                                Time Warner Cable</div></div></div><div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="34">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347059_1404240590.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="40"><div style="">Advisor<br>
+                                Time Warner Cable
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6">
+                        <div class="block image" data-type="image" data-title="image" data-id="34">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347059_1404240590.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="40">
+                            <div style="">Advisor<br>
                                 Jim Ludington<br>
                                 SVP, Technology Bus Ops<br>
-                                Time Warner Cable</div></div></div><div class="column grid_6 omega"><div class="block image" data-type="image" data-title="image" data-id="35">			<div align='left'>
-                                <img src='//cdn.snappages.com/ik387d/assets/668219_2347060_1404240590.png' style='float:left;'  class='image ' /></div>
-                        </div><div class="block text" data-type="text" data-title="text" data-id="43"><div style="">Advisor<br>
+                                Time Warner Cable
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column grid_6 omega">
+                        <div class="block image" data-type="image" data-title="image" data-id="35">
+                            <div align='left'>
+                                <img src="{{asset('img/chapter/668219_2347060_1404240590.png')}}" style='float:left;'  class='image ' />
+                            </div>
+                        </div>
+                        <div class="block text" data-type="text" data-title="text" data-id="43">
+                            <div style="">Advisor<br>
                                 Chris Cardenas<br>
                                 VP, Call Center Support Ops<br>
-                                Time Warner Cable</div></div></div></div></div><div class="column grid_24"><div class="block heading" data-type="heading" data-title="heading" data-id="17"><h3><div class='h3' style='text-align:left;'>Follow Us</div></h3></div><div class="block socialicons" data-type="socialicons" data-title="socialicons" data-id="18"><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='http://www.facebook.com/NAMICCarolinasChapter' target='_blank' class='imageLink'><img src='/resources/site/images/social/facebook-32x32.png' alt='follow on'/></a></div><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='https://twitter.com/#!/NAMICCarolinas' target='_blank' class='imageLink'><img src='/resources/site/images/social/twitter-32x32.png' alt='follow on'/></a></div><div style='float:left;padding-bottom:5px;padding-right:5px;'><a href='http://www.linkedin.com/groups/NAMICCarolinas-4141647?home=&gid=4141647&trk=anet_ug_hm' target='_blank' class='imageLink'><img src='/resources/site/images/social/linkedin-32x32.png' alt='follow on'/></a></div></div><div class="block image" data-type="image" data-title="image" data-id="20">			<div align='left'>
-                        <a href='https://namic.site-ym.com/general/register_member_type.asp' target='_blank' class='imageLink'><img src='//cdn.snappages.com/ik387d/assets/668219_1457822_1404240590.png' style='float:left;'  class='image ' /></a></div>
-                </div></div><div style="clear:both;"></div></div></div>	</div>
+                                Time Warner Cable
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column grid_24">
+                <div class="block heading" data-type="heading" data-title="heading" data-id="17">
+                    <h3>
+                        <div class='h3' style='text-align:left;'>
+                            Follow Us
+                        </div>
+                    </h3>
+                </div>
+                <div class="block socialicons" data-type="socialicons" data-title="socialicons" data-id="18">
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='http://www.facebook.com/NAMICCarolinasChapter' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/facebook-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='https://twitter.com/#!/NAMICCarolinas' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/twitter-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='http://www.linkedin.com/groups/NAMICCarolinas-4141647?home=&gid=4141647&trk=anet_ug_hm' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/linkedin-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                </div>
+                <div class="block image" data-type="image" data-title="image" data-id="20">
+                    <div align='left'>
+                        <a href='https://namic.site-ym.com/general/register_member_type.asp' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/chapter/668219_1457822_1404240590.png')}}" style='float:left;'  class='image ' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>
+</div>
    @endsection
