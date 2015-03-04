1d68c16f0c53a56819cd13cb0ca5e4ee8a694e95
Jon <jon@nocturnalinc.com>
2015-03-04 18:08:38 -0500

    added a stub page for how events were displayed and removed old events from 2014 added 2015 calendar

---
+++ resources/views/main/events-stub.blade.php
@@ -0,0 +1,30 @@
+<div class="block text" data-type="text" data-title="text" data-id="21">
+    <div style="">
+        <p><b>SAVE THE DATE ANNOUNCEMENT</b></p>
+
+        <p><b>First Annual NAMIC-Carolinas Leadership Awards Dinner</b><br>
+            Charlotte, North Carolina<br>
+            <span>September 9, 2014</span><br>
+            <span>7:00 pm</span><br>
+            <span>Awards Granted: Carolinas leaders in the Communications industry.</span></p>
+
+        <p>The EPIC (Exceptional Person in Communications) awards will be our way of recognizing the most outstanding Carolinas Leaders in Communications that support diversity.</p>
+
+        <p><b>Fifth Annual NAMIC-Carolinas Digital Media Summit. “Digital Presence”</b><br>
+            <span>Charlotte, North Carolina</span><br>
+            <span>September 10, 2014</span><br>
+            <span>Registration: 1:30 and the event will start at 2pm.</span></p>
+
+        <p>Keynote Speaker: Angela Talton, SVP, Global Diversity &amp; Inclusion at Nielsen. Jim Ludington will be the Moderator for our panel discussion. Networking mixer immediately following the event.</p>
+    </div>
+</div>
+<div class="block download" data-type="download" data-title="download" data-id="22"><div align="left"><div class="download-button rollover" align="left" style="padding:7px;display:inline-block;" data-fileID="104993" data-url="{{asset('files/events/NAMIC_flyer_2_8 5x11.pdf')}}" data-type="pdf" >
+            <img src="{{asset('img/events/pdf_file_30.png')}}" style="float:left;"/>
+            <img src="{{asset('img/events/cloud_download_25.png')}}" style="margin-top:8px;margin-left:-15px;float:left"/>
+            <div style="float:left;margin-left:5px;margin-top:-2px;">
+                <div style="font-weight:bold;font-size:15px;margin-bottom:-2px;">Save the Date Announcement</div>
+                <a href="{{asset('files/events/NAMIC_flyer_2_8 5x11.pdf')}}">click to download</a>
+            </div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>
