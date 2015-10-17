683451657d14a53fc625ffe1fa0f24ab4aa6039c
bageera <jon@nocturnalinc.com>
2015-10-17 16:07:14 -0400

    updating main page with bootstrap layout and updated banner picture

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,115 +1,112 @@
 @extends('layouts.base')
 @section('title')
     NAMIC-Carolinas - NAMIC-Carolinas Chapter Website
-    @endsection
+@endsection
 @section(('content'))
 
-
     <div id="container">
-        <div id="content" data-layout="twobucketside" data-index="13">
-            <div id="grid" class="container_24">
-                <div id="billboard" class="billboard-holder">
-                    <div class="billboard" data-id="133252" data-title="Home2" data-format="basic" data-height="300" data-transition="fade" data-autoplay="true" data-timing="5" data-hidecontrols="true" data-theme="bevel" data-effect="none" style="height:300px;">
-                        <img src="{{asset('img/main/124145_1787777.jpg')}}" data-loaded="false" data-src="{{asset('img/main/124145_1787777.jpg')}}" alt="" data-customfonts="Arial,Arial" data-source="124145_1787777.jpg" data-heading="Welcome to the NAMIC-Carolinas Website!" data-headingfont="Arial" data-headingsize="30" data-headingbold="true" data-headingitalic="false" data-headingcolor="#FFFFFF" data-headingweight="700" data-headingletterspacing="0" data-caption="" data-captionfont="Arial" data-captionsize="14" data-captionbold="false" data-captionitalic="false" data-captioncolor="#FFFFFF" data-captionweight="400" data-captionletterspacing="0" data-hpos="left" data-vpos="bottom" data-style="black" data-url="" data-target="" data-offset="-9" data-tint="rgba(255,50,50,0)" />
-                    </div>
-                </div>
-                <div class="column container grid_24 alpha">
-                    <div class="column container grid_16 alpha">
-                        <div class="column grid_16 alpha">
-                            <div class="block heading" data-type="heading" data-title="heading" data-id="0">
-                                <h3>
-                                    <div class='h3' style='text-align:left;'>Building the Next Generation of Leaders
-                                    </div>
-                                </h3>
-                            </div>
-                            <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="1">
-                                <img src='{{asset('img/main/716743_1456350_1401288004.png')}}' style='float:left;'  class='image' />
-                                <div style="">
-                                    NAMIC-Carolinas is an Award-Winning chapter serving North and South Carolina.
-                                    We provide programming that helps to meet the mission of our national chapter NAMIC (National Association for Multi-ethnicity in Communications) to educate,
-                                    advocate and empower for multi-ethnic diversity within the communications industry.<br><br>
-                                    With signature programs such as Lunch and Learns, Digital Media Summits and Career Development Workshops,
-                                    we keep you ahead of the curve with how technology is shaping our industry and creative ways to be prepared professionally.<br><br>
-                                    Please explore our website to learn more about how NAMIC-Carolinas could assist you with growth and development professionally!
-                                </div>
-                            </div>
-                            <div class="block divider" data-type="divider" data-title="divider" data-id="2">
-                                <div class="divider"></div>
-                            </div>
-                        </div>
-                        <div class="column grid_8 alpha"><div class="block image" data-type="image" data-title="image" data-id="3">
-                                <div align='center'>
-                                    <img src="{{asset('img/main/716743_1635910_1401288004.jpg')}}" align='center'  class='image ' />
-                                </div>
-                            </div>
-                            <div class="block text" data-type="text" data-title="text" data-id="4">
-                                <div style=""><div>
-                                        <a aria-describedby="ui-tooltip-17" href="http://www.namic.com/newsletters/wpdev/qa-with-garth-david-director-finance-time-warner-cable-and-namic-carolinas-president/" target="_self">Q&amp;A with Our NAMIC-Carolinas Advisor, David Garth </a></div>
-                                </div>
-                            </div>
-                        </div>
 
-                    </div>
-                    <div class="column grid_8 omega">
-                        <div class="block heading" data-type="heading" data-title="heading" data-id="7">
-                            <h3>
-                                <div class='h3' style='text-align:left;'>Follow Us
-                                </div>
-                            </h3>
-                        </div>
-                        <div class="block text" data-type="text" data-title="text" data-id="8">
-                            <div style="">
-                                NAMIC-Carolinas can be found on Facebook, Twitter and LinkedIn. Be sure to follow and Like us today!
-                            </div>
-                        </div>
-                        <div class="block socialicons" data-type="socialicons" data-title="socialicons" data-id="9">
-                            <div style='float:left;padding-bottom:5px;padding-right:5px;'>
-                                <a href='http://www.facebook.com/NAMICCarolinasChapter' target='_blank' class='imageLink'>
-                                    <img src="{{asset('img/main/facebook-32x32.png')}}" alt='follow on'/>
-                                </a>
-                            </div>
-                            <div style='float:left;padding-bottom:5px;padding-right:5px;'>
-                                <a href='https://twitter.com/#!/NAMICCarolinas' target='_blank' class='imageLink'>
-                                    <img src="{{asset('img/main/twitter-32x32.png')}}" alt='follow on'/>
-                                </a>
-                            </div>
-                            <div style='float:left;padding-bottom:5px;padding-right:5px;'>
-                                <a href='http://www.linkedin.com/groups/NAMICCarolinas-4141647?home=&gid=4141647&trk=anet_ug_hm' target='_blank' class='imageLink'>
-                                    <img src="{{asset('img/main/linkedin-32x32.png')}}" alt='follow on'/>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="block heading" data-type="heading" data-title="heading" data-id="10">
-                            <h3>
-                                <div class='h3' style='text-align:left;'>
-                                    Become a Member
-                                </div>
-                            </h3>
-                        </div>
-                        <div class="block image" data-type="image" data-title="image" data-id="11">
-                            <div align='left'>
-                                <a href='https://namic.site-ym.com/general/register_member_type.asp' target='_self' class='imageLink'>
-                                    <img src="{{asset('img/main/716743_1457817_1401288004.png')}}" style='float:left;'  class='image ' />
-                                </a>
-                            </div>
-                        </div>
-                        <div class="block image" data-type="image" data-title="image" data-id="20">
-                            <div align='left'>
-                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-                                    <input type="hidden" name="cmd" value="_s-xclick">
-                                    <input type="hidden" name="hosted_button_id" value="CFQQTDWGZP7CS">
-                                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                                    <img alt="" border="0" src="" width="1" height="1">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div style="clear:both;"></div>
+        <div class="row">
+            <!-- banner -->
+            <div class="col-md-16">
+
+                <img src="{{asset('img/main/untitled.png')}}" data-loaded="false" data-src="{{asset('img/main/untitled.png')}}">
+            </div>
+            <div class="col-md-8">
+                <h3 class="h3">
+                    Building the Next Generation of Leaders
+
+                </h3>
+
+            </div>
+            <div class="col-md-4">
+                <h3 class="h3">
+                    Follow Us
+
+                </h3>
+
             </div>
         </div>
+        <div class="row">
+
+            <div class="block imagetext col-md-8" data-type="imagetext" data-title="imagetext" data-id="1">
+                <img src='{{asset('img/main/716743_1456350_1401288004.png')}}' style='float:left;'  class='image' />
+                <div style="">
+                    NAMIC-Carolinas is an Award-Winning chapter serving North and South Carolina.
+                    We provide programming that helps to meet the mission of our national chapter NAMIC (National Association for Multi-ethnicity in Communications) to educate,
+                    advocate and empower for multi-ethnic diversity within the communications industry.<br><br>
+                    With signature programs such as Lunch and Learns, Digital Media Summits and Career Development Workshops,
+                    we keep you ahead of the curve with how technology is shaping our industry and creative ways to be prepared professionally.<br><br>
+                    Please explore our website to learn more about how NAMIC-Carolinas could assist you with growth and development professionally!
+                </div>
+
+
+                <div class="divider"></div>
+                <img src="{{asset('img/main/716743_1635910_1401288004.jpg')}}" align='center'  class='image ' />
+                <div style=""><div>
+                        <a aria-describedby="ui-tooltip-17" href="http://www.namic.com/newsletters/wpdev/qa-with-garth-david-director-finance-time-warner-cable-and-namic-carolinas-president/" target="_self">Q&amp;A with Our NAMIC-Carolinas Advisor, David Garth </a></div>
+                </div>
+            </div>
+
+
+
+            <!-- right sidebar -->
+            <div class="col-md-4">
+                <div class="block text" data-type="text" data-title="text" data-id="8">
+                    <div style="">
+                        NAMIC-Carolinas can be found on Facebook, Twitter and LinkedIn. Be sure to follow and Like us today!
+                    </div>
+                </div>
+                <div class="block socialicons" data-type="socialicons" data-title="socialicons" data-id="9">
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='http://www.facebook.com/NAMICCarolinasChapter' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/facebook-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='https://twitter.com/#!/NAMICCarolinas' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/twitter-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                    <div style='float:left;padding-bottom:5px;padding-right:5px;'>
+                        <a href='http://www.linkedin.com/groups/NAMICCarolinas-4141647?home=&gid=4141647&trk=anet_ug_hm' target='_blank' class='imageLink'>
+                            <img src="{{asset('img/main/linkedin-32x32.png')}}" alt='follow on'/>
+                        </a>
+                    </div>
+                </div>
+                <div class="block heading" data-type="heading" data-title="heading" data-id="10">
+                    <h3 class="h3">
+
+                        Become a Member
+
+                    </h3>
+                </div>
+                <div class="block image" data-type="image" data-title="image" data-id="11">
+                    <div align='left'>
+                        <a href='https://namic.site-ym.com/general/register_member_type.asp' target='_self' class='imageLink'>
+                            <img src="{{asset('img/main/716743_1457817_1401288004.png')}}" style='float:left;'  class='image ' />
+                        </a>
+                    </div>
+                </div>
+                <div class="block image" data-type="image" data-title="image" data-id="20">
+                    <div align='left'>
+                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="hosted_button_id" value="CFQQTDWGZP7CS">
+                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                            <img alt="" border="0" src="" width="1" height="1">
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
     </div>
 
 @endsection
-
-
