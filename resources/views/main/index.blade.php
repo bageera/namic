b43888aeb9df402ae1f70b7996dee9e01dca143f
bageera <jon@nocturnalinc.com>
2015-11-19 07:43:30 -0500

    fix type of headnammer to png

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -10,7 +10,7 @@
             <!-- banner -->
             <div class="col-md-16">
 
-                <img src="{{asset('img/main/headbanner.jpg')}}" data-loaded="false" data-src="{{asset('img/main/headbanner.jpg')}}">
+                <img src="{{asset('img/main/headbanner.png')}}" data-loaded="false" data-src="{{asset('img/main/headbanner.png')}}">
             </div>
             <div class="col-md-8">
                 <h3 class="h3">
