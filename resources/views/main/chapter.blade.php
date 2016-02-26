a618977f812007c6a74e48a57f2363420507c83b
bageera <jon@nocturnalinc.com>
2016-02-26 14:12:54 -0500

    updated vice president headshot

--- resources/views/main/chapter.blade.php
+++ resources/views/main/chapter.blade.php
@@ -40,7 +40,7 @@
                     </div>
                     <div class="column grid_6"><div class="block image" data-type="image" data-title="image" data-id="22">
                             <div align='left'>
-                                <img src="{{asset('http://placehold.it/205x277')}}" style='float:left;'  class='image ' />
+                                <img src="{{asset('img/chapter/garthdavid.jpg')}}" style='float:left;'  class='image ' />
                             </div>
                         </div>
                         <div class="block text" data-type="text" data-title="text" data-id="27">
