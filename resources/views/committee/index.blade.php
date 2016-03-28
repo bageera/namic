9a4717010c6d5ead6acf2278d4b5c7d73400609e
bageera <jon@nocturnalinc.com>
2016-03-28 14:30:06 -0400

    brief layout for committee page

--- resources/views/committee/index.blade.php
+++ resources/views/committee/index.blade.php
@@ -4,4 +4,93 @@
 @endsection
 @section(('content'))
 
+    <div id="container">
+
+
+        <div id="pageTitle" class="page-title">Namic Committees</div><br class="page-title" />
+        <div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Marketing / Communication </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description
+                        </p>
+
+                    </div>
+                    <div class="panel-footer"><b>Position Name</b> Contact(email address or Name)
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Membership </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description
+                        </p>
+
+                    </div>
+                    <div class="panel-footer"><b>Position Name</b> Contact(email address or Name)
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Programming </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description
+                        </p>
+
+                    </div>
+                    <div class="panel-footer"><b>Position Name</b> Contact(email address or Name)
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Mentoring / Scholarship Chair </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description
+                        </p>
+
+                    </div>
+                    <div class="panel-footer"><b>Position Name</b> Contact(email address or Name)
+
+                    </div>
+
+                </div>
+
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Revenue Generation </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description
+                        </p>
+
+                    </div>
+                    <div class="panel-footer"><b>Position Name</b> Contact(email address or Name)
+
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+
 @endsection
