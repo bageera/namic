67500cba40d73c727257699e27406955d61285c5
bageera <jon@nocturnalinc.com>
2016-03-28 12:43:59 -0400

    updated images for board

--- resources/views/main/chapter.blade.php
+++ resources/views/main/chapter.blade.php
@@ -52,7 +52,7 @@
                     <div class="column grid_6">
                         <div class="block image" data-type="image" data-title="image" data-id="23">
                             <div align='left'>
-                                <img src="{{asset('http://placehold.it/205x277')}}" style='float:left;'  class='image ' />
+                                <img src="{{asset('img/chapter/ccuevas.jpg')}}" style='float:left;'  class='image ' />
                             </div>
                         </div>
                         <div class="block text" data-type="text" data-title="text" data-id="28">
