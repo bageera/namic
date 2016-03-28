3ebf2b7118d40ae3c9701408da5c2c24190f172c
bageera <jon@nocturnalinc.com>
2016-03-28 14:34:36 -0400

    updating cipher and pushing committeecontroller

---
+++ app/Http/Controllers/CommitteeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+
+
+class CommitteeController extends Controller
+{
+
+
+    public function index()
+    {
+
+        return view('committee.index');
+    }
+
+}
