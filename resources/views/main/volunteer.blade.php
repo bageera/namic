f7dad5690ee00fbe7775977a9bc9b5971324e691
bageera <jon@nocturnalinc.com>
2016-05-10 00:55:55 -0400

    added volunteering page

---
+++ resources/views/main/volunteer.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.base')
+@section('title')
+    NAMIC-Carolinas - Epic Awards
+@endsection
+@section(('content'))
+
+
+    <div id="container">
+
+        <div id="pageTitle" class="page-title">Namic Carolinas Committees</div><br class="page-title" />
+        <div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Marketing / Communication marketing&#64;namiccarolinas.com</div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description will be coming soon
+                        </p>
+                        <p><b>Positions Available</b></p>
+
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Membership membership&#64;namiccarolinas.com</div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description will be coming soon
+                        </p>
+                        <p><b>Positions Available</b></p>
+
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Programming programming&#64;namiccarolinas.com</div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description will be coming soon
+                        </p>
+                        <p><b>Positions Available</b></p>
+
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+
+                </div>
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Mentoring / Scholarship Chair mentoring&#64;namiccarolinas.com</div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description will be coming soon
+                        </p>
+                        <p><b>Positions Available</b></p>
+
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+
+                </div>
+
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Revenue Generation </div>
+                    <div class="panel-body">
+                        <p>
+                            Committee Description will be coming soon
+                        </p>
+                        <p><b>Positions Available</b></p>
+
+                    </div>
+                    <div class="panel-footer">
+
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
+
