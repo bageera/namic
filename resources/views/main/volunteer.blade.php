c06ad1473a5afcd44dc640c75914f1519d88e8ad
bageera <jon@nocturnalinc.com>
2016-05-12 21:51:38 -0400

    iupdated css

--- resources/views/main/volunteer.blade.php
+++ resources/views/main/volunteer.blade.php
@@ -7,94 +7,33 @@
 
     <div id="container">
 
-        <div id="pageTitle" class="page-title">Namic Carolinas Committees</div><br class="page-title" />
-        <div>
+        <div class="col-lg-12">
+            <div id="pageTitle" class="page-title">Volunteer with NAMIC</div><br class="page-title" /></div>
+            <div class="block heading" data-type="heading" data-title="heading" data-id="2">
 
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                If any of the available position interest you feel free to reach out to us on our <a href="{{URL('contact')}}">contact page</a>
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Marketing / Communication </div>
-                    <div class="panel-body">
-                        <p>
-                            Committee Description will be coming soon
-                        </p>
-                        <p><b>Positions Available</b></p>
-
+                    <div class='h3' style='text-align:left;'>
+                        2016 Get Active
                     </div>
-                    <div class="panel-footer">
-
-                    </div>
-
-                </div>
-
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Membership</div>
-                    <div class="panel-body">
-                        <p>
-                            Committee Description will be coming soon
-                        </p>
-                        <p><b>Positions Available</b></p>
-
-                    </div>
-                    <div class="panel-footer">
-
-                    </div>
-
-                </div>
-
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Programming</div>
-                    <div class="panel-body">
-                        <p>
-                            Committee Description will be coming soon
-                        </p>
-                        <p><b>Positions Available</b></p>
-
-                    </div>
-                    <div class="panel-footer">
-
-                    </div>
-
-                </div>
-
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Mentoring / Scholarship Chair</div>
-                    <div class="panel-body">
-                        <p>
-                            Committee Description will be coming soon
-                        </p>
-                        <p><b>Positions Available</b></p>
-
-                    </div>
-                    <div class="panel-footer">
-
-                    </div>
-
-                </div>
-
-
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Revenue Generation </div>
-                    <div class="panel-body">
-                        <p>
-                            Committee Description will be coming soon
-                        </p>
-                        <p><b>Positions Available</b></p>
-
-                    </div>
-                    <div class="panel-footer">
-
-                    </div>
-
-                </div>
-
 
             </div>
-        </div>
+
+            <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="3">
+                <img src="{{asset('img/main/volunteer.jpg')}}" style='float:right;padding-left:20px;padding-right:0;'  class='image' />
+                <div style="">
+                    Starting in 2016 NAMIC will have unique volunteer opportunities available with the organization.
+
+
+
+                </div>
+            </div>
 
     </div>
+
+
+
+
+
+
 
 @endsection
 
