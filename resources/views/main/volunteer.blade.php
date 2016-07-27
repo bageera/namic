640b1549fb8eb4dd2a24f720f8c64428c0eb2004
bageera <jon@nocturnalinc.com>
2016-07-26 23:32:31 -0400

    updating volunteering page to remove old opportunities

--- resources/views/main/volunteer.blade.php
+++ resources/views/main/volunteer.blade.php
@@ -12,28 +12,14 @@
             <div class="block heading" data-type="heading" data-title="heading" data-id="2">
 
                     <div class='h3' style='text-align:left;'>
-                        Proctors Needed
+                        Stay Tuned
                     </div>
 
             </div>
 
             <div class="block imagetext" data-type="imagetext" data-title="imagetext" data-id="3">
-                <img src="{{asset('img/volunteer/flyer.jpg')}}" style='float:right;padding-left:20px;padding-right:0;'  class='image' />
-                <div style="">
-                    <p>
-                        We have an opportunity for volunteering at Druid Hills Academy for End of Grade Testing!!! from the 6th of June till the 10th
-                    </p>
-                    <p>
-                        If this will be your first time as a proctor we will have training sessions <b>May 27<sup>th</sup></b> at <b>10:00 AM</b>, <b>Noon</b>, and <b>2:00 PM</b>. Training
-                        sessions will be approx 40 minutes
-                    </p>
-                    <p>
-                        If you intend on volunteering you must register as a cmsvolunteer at <a href="https://www.cmsvolunteers.com">https://www.cmsvolunteers.com</a> which takes about 4 minutes.
-                    </p>
+                Book mark our page and check back for upcoming volunteering opportunities
 
-
-
-                </div>
             </div>
 
     </div>
