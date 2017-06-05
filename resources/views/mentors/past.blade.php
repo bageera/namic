8b575588e7861d387cae1393f77a9e4770aad1eb
bageera <jon@nocturnalinc.com>
2017-06-05 09:13:31 -0400

    updating mentor of the month and adding past mentor

--- resources/views/mentors/past.blade.php
+++ resources/views/mentors/past.blade.php
@@ -9,6 +9,24 @@
             <div class="panel-body">
 
                 <div class="col-md-8">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/zKo6gJnf-6E" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="col-md-4">
+                    <p>Charlon McIntosh - "Don't Major in Minor"</p>
+                    <p>May 2017</p>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+        <div class="panel panel-primary">
+            <div class="panel-heading">Mentor Minute <b>April 2017</b></div>
+            <div class="panel-body">
+
+                <div class="col-md-8">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/phyo88wexCo" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-4">
