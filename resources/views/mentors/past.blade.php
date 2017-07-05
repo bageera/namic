e86625aee94c75103e1336b2f1bb8f5a5a7e420e
Jon Bethea <jonbethea@jbethea-m15mbpr.local>
2017-07-05 13:14:54 -0400

    adding june mentor to the past members list

--- resources/views/mentors/past.blade.php
+++ resources/views/mentors/past.blade.php
@@ -5,6 +5,24 @@
 @section(('content'))
     <div id="container">
         <div class="panel panel-primary">
+            <div class="panel-heading">Mentor Minute</div>
+            <div class="panel-body">
+
+                        <div class="col-md-8">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/SG5411mkT44" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        <div class="col-md-4">
+                            <p>Xavion Newkirk - "Next Level Commitment"</p>
+                            <p>June 2017</p>
+                        </div>
+
+
+
+
+            </div>
+
+        </div>
+        <div class="panel panel-primary">
             <div class="panel-heading">Mentor Minute <b>April 2017</b></div>
             <div class="panel-body">
 
