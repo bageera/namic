34321cd3621dc410341f3d4837350917019b04bd
bageera <jon@nocturnalinc.com>
2017-07-13 00:36:21 -0400

    updated thank you page

--- resources/views/emails/epic/thank-you.blade.php
+++ resources/views/emails/epic/thank-you.blade.php
@@ -1,5 +1,5 @@
 <p>
-    Thank you for your nomination and submission  <strong>{{ $nominator_name }}</strong> for the 3rd Annual NAMIC CAROLINAS EPIC Awards
+    Thank you very much for taking the time to nominate  <strong>{{ $nominator_name }}</strong> for the 4rd Annual NAMIC CAROLINAS EPIC Awards
 </p>
 <p>
     The information will be forwarded to the NAMIC-Carolinas board for consideration.
